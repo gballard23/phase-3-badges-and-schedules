5767ddb000a2adf4af8aa05822a2fffcdd340187
Gerren <g.ballard23@outlook.com>
2022-08-29 12:01:05 -0500

    commit tree 95eb8ee97790d0b4f7d44a317f7aff51ac89d61d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map do |i|
+        badge_maker(i)
+    end
+end
+
+def assign_rooms arr 
+    arr.map.with_index do |arr, index|
+        "Hello, #{arr}! You'll be assigned to room #{index + 1}!"
+    end
+end
+     
+def printer arr
+    batch_badge_creator(arr).each do |i|
+        puts i
+    end
+    assign_rooms(arr).each do |i|
+        puts i
+    end
+end
+
